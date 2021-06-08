433b407d07833f6b811ffc162153892af25bf03d
adhietya <rafflieadhietya13@gmail.com>
2021-06-09 02:45:35 +0700

    Membuat Main Class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Muhammad";
+        String namaTengah = "Adhietya";
+        String namaBelakang = "Rafflie";
+
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.8889;
+        char nilaiAbjad = 'A';
+        boolean Tampan = true;
+
+        System.out.println(namaDepan+namaTengah+namaBelakang);
+    }
+}
