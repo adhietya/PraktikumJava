e7ff78282591e8a860ac4e94ece234706d7d02b2
adhietya <rafflieadhietya13@gmail.com>
2021-06-09 16:06:57 +0700

    Operasi Tipe Data String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import sun.plugin.cache.OldCacheEntry;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Muhammad";
@@ -16,5 +21,23 @@ public class Main {
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
         System.out.println(uniskaChar);
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+
+        System.out.println(namaLengkap);
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("fli"));
+        System.out.println(namaLengkap.substring(8));
+        System.out.println(namaLengkap.substring(8,13));
+        System.out.println(namaLengkap.replace( "Adh", "Radh"));
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.charAt(9));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama: namaArray)
+            System.out.println(nama);
+
     }
 }
