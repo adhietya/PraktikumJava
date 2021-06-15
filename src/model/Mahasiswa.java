1384fc287f24ab675aeff23c40b71b5ad66da6e2
adhietya <rafflieadhietya13@gmail.com>
2021-06-15 12:04:44 +0700

    Method void

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -1,9 +1,20 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Mahasiswa {
     String npm;
     String nama;
     Date tanggalLahir;
+
+    void tampilkanAtribut(){
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(polaTanggal);
+
+            System.out.println("NPM \t\t\t : " + this.npm);
+            System.out.println("Nama \t\t\t : " + this.nama);
+            System.out.println("Tanggal Lahir \t : " + sdf.format(this.tanggalLahir));
+    }
 }
--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -18,10 +18,7 @@ public class MahasiswaBeraksi {
         mahasiswa.nama = "Awal";
         try {
             mahasiswa.tanggalLahir = sdf.parse("02-01-2000");
-
-            System.out.println("NPM \t\t\t : " + mahasiswa.npm);
-            System.out.println("Nama \t\t\t : " + mahasiswa.nama);
-            System.out.println("Tanggal Lahir \t : " + sdf.format(mahasiswa.tanggalLahir));
+            mahasiswa.tampilkanAtribut();
         } catch (ParseException e) {
             System.err.println("Format tanggal tidak sesuai");
         }
