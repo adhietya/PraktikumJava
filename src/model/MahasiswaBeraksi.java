3a31b5037c56313b5a080659615d804181d73434
adhietya <rafflieadhietya13@gmail.com>
2021-06-15 12:41:39 +0700

    Hitung usia

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -7,21 +7,21 @@ import java.util.Date;
 public class MahasiswaBeraksi {
     public static void main(String[] args){
 
-
        String polaTanggal = "dd-MM-yyyy";
         SimpleDateFormat sdf = new SimpleDateFormat(polaTanggal);
-
 
         //instance - pembuatan object
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19631003";
         mahasiswa.nama = "Awal";
         try {
-            mahasiswa.tanggalLahir = sdf.parse("02-01-2000");
+            mahasiswa.tanggalLahir = sdf.parse("09-06-2000");
             mahasiswa.tampilkanAtribut();
         } catch (ParseException e) {
             System.err.println("Format tanggal tidak sesuai");
         }
+
+        System.out.println();
 
 
 
